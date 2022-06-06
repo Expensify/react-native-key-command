ed7a25205a570e6e090c9edbdef825db71c28df9
azimgd <me@azimgd.com>
2022-06-06 16:14:37 +0500

    Implement Hardware KeyCommand event listeners

--- ios/KeyCommand.h
+++ ios/KeyCommand.h
@@ -1,5 +1,32 @@
 #import <React/RCTBridgeModule.h>
+#import <UIKit/UIKit.h>
 
 @interface KeyCommand : NSObject <RCTBridgeModule>
+
+@end
+
+
+@interface HardwareKeyCommands : NSObject
+
++ (instancetype)sharedInstance;
+
+/**
+ * Register a keyboard command.
+ */
+- (void)registerKeyCommandWithInput:(NSString *)input
+                      modifierFlags:(UIKeyModifierFlags)flags
+                             action:(void (^)(UIKeyCommand *command))block;
+
+/**
+ * Unregister a keyboard command.
+ */
+- (void)unregisterKeyCommandWithInput:(NSString *)input
+                        modifierFlags:(UIKeyModifierFlags)flags;
+
+/**
+ * Check if a command is registered.
+ */
+- (BOOL)isKeyCommandRegisteredForInput:(NSString *)input
+                         modifierFlags:(UIKeyModifierFlags)flags;
 
 @end
