3a476cb301c2acd225b78befebb7683df7e26b97
azimgd <me@azimgd.com>
2022-08-15 15:57:29 +0500

    chore: eslint fix

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  dependencies: {
-    'react-native-key-command': {
-      root: path.join(__dirname, '..'),
+    dependencies: {
+        'react-native-key-command': {
+            root: path.join(__dirname, '..'),
+        },
     },
-  },
 };
