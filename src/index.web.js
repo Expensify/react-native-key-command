7ef9cb632b1184a0d4548bb0db502808a75c6075
azimgd <me@azimgd.com>
2022-11-04 16:38:04 +0500

    feat: setup dev version of library dependency inclusion via craco for dev setup

---
+++ src/index.web.js
@@ -0,0 +1,38 @@
+const constants = {};
+const eventEmitter = {};
+
+// function validateKeyCommand(keyCommand) {
+
+// }
+
+// function validateKeyCommands(keyCommands) {
+
+// }
+
+// function getConstants() {
+
+// }
+
+function registerKeyCommands(keyCommands) {
+
+}
+
+function unregisterKeyCommands(keyCommands) {
+
+}
+
+// function getEventEmitter() {
+
+// }
+
+function addListener(keyCommand, callback) {
+
+}
+
+export {
+    registerKeyCommands,
+    unregisterKeyCommands,
+    constants,
+    eventEmitter,
+    addListener,
+};
